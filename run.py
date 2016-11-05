a01ec30468306a81b4d986d7fae16c5097fd1dbb
MatthewDooler <MatthewDooler@users.noreply.github.com>
2016-11-05 17:43:18 +0000

    parse the html

--- run.py
+++ run.py
@@ -8,10 +8,17 @@ headers = {
 
 request_uri = "https://beta.tfgm.com/public-transport/stations/mediacityuk-tram"
 
+print "Making HTTP request to " + request_uri
 response = requests.get(request_uri, headers=headers)
 html_doc = response.text
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-print html_doc
+depatures_tbody = soup.findAll('tbody', {"id" : "departure-items"})[0]
+tram_trs = depatures_tbody.findAll('tr',  {"class" : "tram"})
 
-#conversation_divs = soup.findAll('div', {"class" : "conversation"})
+for tram_tr in tram_trs:
+	destination = tram_tr.findAll('td',  {"class" : "departure-destination"})[0].text
+	wait_td = tram_tr.findAll('td',  {"class" : "departure-wait"})[0]
+	wait_figure = wait_td.findAll('span',  {"class" : "figure"})[0].text
+	wait_unit = wait_td.findAll('span',  {"class" : "unit"})[0].text
+	print "Tram to " + destination + " leaves in " + wait_figure + " " + wait_unit

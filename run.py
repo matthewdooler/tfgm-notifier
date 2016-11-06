035c641f892776d36d0080a6c819fe46990baac5
MatthewDooler <MatthewDooler@users.noreply.github.com>
2016-11-06 09:02:39 +0000

    play audio

--- run.py
+++ run.py
@@ -11,56 +11,59 @@ headers = {
 request_uri = "https://beta.tfgm.com/public-transport/stations/mediacityuk-tram"
 audio_dir = "audio"
 
-print "Making HTTP request to " + request_uri
-response = requests.get(request_uri, headers=headers)
-html_doc = response.text
-soup = BeautifulSoup(html_doc, 'html.parser')
+while True:
 
-depatures_tbody = soup.findAll('tbody', {"id" : "departure-items"})[0]
-tram_trs = depatures_tbody.findAll('tr',  {"class" : "tram"})
+	print "Making HTTP request to " + request_uri
+	response = requests.get(request_uri, headers=headers)
+	html_doc = response.text
+	soup = BeautifulSoup(html_doc, 'html.parser')
 
-trams = []
-for tram_tr in tram_trs:
-	destination = tram_tr.findAll('td',  {"class" : "departure-destination"})[0].text
-	wait_td = tram_tr.findAll('td',  {"class" : "departure-wait"})[0]
-	wait_figure = wait_td.findAll('span',  {"class" : "figure"})[0].text
-	wait_unit = wait_td.findAll('span',  {"class" : "unit"})[0].text
-	trams.append({"destination": destination, "wait_figure": wait_figure, "wait_unit": wait_unit})
-print "Trams:"
-print trams
-print ""
+	depatures_tbody = soup.findAll('tbody', {"id" : "departure-items"})[0]
+	tram_trs = depatures_tbody.findAll('tr',  {"class" : "tram"})
 
-interesting_destinations = ["Piccadilly", "See Tram Front"]
+	trams = []
+	for tram_tr in tram_trs:
+		destination = tram_tr.findAll('td',  {"class" : "departure-destination"})[0].text
+		wait_td = tram_tr.findAll('td',  {"class" : "departure-wait"})[0]
+		wait_figure = wait_td.findAll('span',  {"class" : "figure"})[0].text
+		wait_unit = wait_td.findAll('span',  {"class" : "unit"})[0].text
+		trams.append({"destination": destination, "wait_figure": wait_figure, "wait_unit": wait_unit})
+	print "Trams:"
+	print trams
+	print ""
 
-interesting_trams = []
-for tram in trams:
-	if tram["destination"] in interesting_destinations:
-		interesting_trams.append(tram)
-print "Interesting trams:"
-print interesting_trams
-print ""
+	interesting_destinations = ["Piccadilly", "See Tram Front"]
 
-min_wait = 2
-max_wait = 12
-interesting_trams_within_acceptable_wait = []
-for tram in interesting_trams:
-	wait = int(tram["wait_figure"])
-	if wait <= max_wait and wait >= min_wait:
-		interesting_trams_within_acceptable_wait.append(tram)
+	interesting_trams = []
+	for tram in trams:
+		if tram["destination"] in interesting_destinations:
+			interesting_trams.append(tram)
+	print "Interesting trams:"
+	print interesting_trams
+	print ""
 
-print "Interesting trams within acceptable wait:"
-print interesting_trams_within_acceptable_wait
-print ""
+	min_wait = 2
+	max_wait = 12
+	interesting_trams_within_acceptable_wait = []
+	for tram in interesting_trams:
+		wait = int(tram["wait_figure"])
+		if wait <= max_wait and wait >= min_wait:
+			interesting_trams_within_acceptable_wait.append(tram)
 
-def play_audio(key):
-	os.system("afplay " + audio_dir + "/"+key+".mp3")  
+	print "Interesting trams within acceptable wait:"
+	print interesting_trams_within_acceptable_wait
+	print ""
 
-for tram in interesting_trams_within_acceptable_wait:
-	play_audio("the-next-tram-to")
-	destination_key = tram["destination"].replace(" ", "-").lower()
-	play_audio(destination_key)
-	play_audio("departs-in")
-	wait_key = tram["wait_figure"].replace(" ", "-").lower()
-	play_audio(wait_key)
-	play_audio("minutes")
-	time.sleep(5)
+	def play_audio(key):
+		os.system("afplay " + audio_dir + "/"+key+".mp3")  
+
+	for tram in interesting_trams_within_acceptable_wait:
+		play_audio("the-next-tram-to")
+		destination_key = tram["destination"].replace(" ", "-").lower()
+		play_audio(destination_key)
+		play_audio("departs-in")
+		wait_key = tram["wait_figure"].replace(" ", "-").lower()
+		play_audio(wait_key)
+		play_audio("minutes")
+		time.sleep(5)
+	time.sleep(60)
